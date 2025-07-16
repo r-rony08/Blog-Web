b1ffba4646ac6b7336cf16d43ee43d1877e88d62
r-rony08 <robiul.rony1008@gmail.com>
2025-07-16 23:07:48 +0600

    Comment and Like function create

--- blog/App_Blog/views.py
+++ blog/App_Blog/views.py
@@ -5,6 +5,7 @@ from .models import Blog, Comment, Like
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
+from App_Blog.forms import CommentForm
 
 # Create your views here.
 
@@ -30,5 +31,37 @@ class CreateBlog(LoginRequiredMixin, CreateView):
 
 def blog_details(request, slug):
     blog = Blog.objects.get(slug=slug)
+    comment_form = CommentForm()
 
-    return render(request, 'App_Blog/blog_details.html', context={'blog':blog})
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.blog = blog
+            comment.save()
+            return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':slug}))
+
+
+    return render(request, 'App_Blog/blog_details.html', context={'blog':blog, 'comment_form':comment_form})
+
+
+@login_required
+def liked(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Like.objects.filter(blog=blog, user=user)
+
+    if not already_liked:
+        liked_post = Like(blog=blog, user=user)
+        liked_post.save()
+    return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':slug}))
+
+
+@login_required
+def unliked(request, pk):
+    blog = Blog.objects.get(pk=pk)
+    user = request.user
+    already_liked = Like.objects.filter(blog=blog, user=user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug':slug}))
